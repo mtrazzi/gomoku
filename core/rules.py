3ee23e338effcdde1a64598d4ce0f9494a79f628
kcosta42 <kcosta@student.42.fr>
2019-12-05 11:34:24 +0100

    Finish all rules

--- core/rules.py
+++ core/rules.py
@@ -4,6 +4,25 @@ class Rules(object):
   def __init__(self):
     return
 
+  def can_capture(self, board, x, y, player):
+    for offset_x, offset_y in COORDS_LIST:
+      bound_x = x + offset_x * 3
+      bound_y = y + offset_y * 3
+      if bound_x < 0 or bound_x >= board.size \
+      or bound_y < 0 or bound_y >= board.size:
+        continue
+      if not board.is_stone(bound_x, bound_y, player):
+        continue
+
+      stone1_x, stone1_y = x + offset_x,     y + offset_y
+      stone2_x, stone2_y = x + offset_x * 2, y + offset_y * 2
+      if  not board.is_stone(stone1_x, stone1_y, player) \
+      and not board.is_stone(stone2_x, stone2_y, player) \
+      and not board.is_empty(stone1_x, stone1_y)         \
+      and not board.is_empty(stone2_x, stone2_y):
+        return True
+    return False
+
   def capture(self, board, player):
     """Manage the Capture Rule
     Only two stone can be "captured" at the same time only if they are
@@ -125,10 +144,30 @@ class Rules(object):
         i += 1
     return threes <= 1
 
+  def check_captures(self, board, player):
+    for i in range(board.size):
+      for j in range(board.size):
+        if self.can_capture(board, i, j, player):
+          return True
+    return False
+
   def check_winner(self, board, players):
     for player in players:
-      if self.aligned_win(board, player):
+      if player.captures >= 10:
         return player
+
+    if self.aligned_win(board, players[0]):
+      if not self.check_captures(board, players[1]) and players[1] < 8:
+        return players[0]
+      else:
+        return players[1]
+
+    if self.aligned_win(board, players[1]):
+      if not self.check_captures(board, players[0]) and players[0] < 8:
+        return players[1]
+      else:
+        return players[0]
+
     return None
 
 COORDS_LIST = [
