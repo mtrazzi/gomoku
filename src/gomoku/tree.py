1c524435421e9e04f2e3c44d872b9ce24c810e64
mtrazzi <michael.trazzi@gmail.com>
2020-01-17 14:36:15 +0100

    cosmetic fixes

--- src/gomoku/tree.py
+++ src/gomoku/tree.py
@@ -1,7 +1,7 @@
 from anytree import Node, RenderTree
 import numpy as np
 
-from gomoku.utils import ucb
+from gomoku.utils import get_attr, ucb
 
 
 class Tree(Node):
@@ -15,17 +15,11 @@ class Tree(Node):
     if children:
         self.children = children
 
-  def is_child(self):
-    return len(self.children) > 0
-
-  def increment_visits(self):
-    self.n_visits += 1
-
   def traverse_one(self, move, n_visits=1, value=1):
-    if self.is_child():
+    if not self.is_leaf:
       for child in self.children:
         if child.name == str(move):
-          child.increment_visits()
+          self.n_visits += 1
           return child
     test = Tree(move, n_visits=n_visits, parent=self)
     return test
@@ -35,17 +29,17 @@ class Tree(Node):
             self.children]
 
   def attr_list(self, attr_name):
-    def _get_attr(node): return getattr(node, attr_name)
-    return list(map(_get_attr, self.children))
+    return list(map(get_attr(attr_name), self.children))
 
   def most_attr_child(self, attr_name):
     return self.children[np.argmax(self.attr_list(attr_name))].move
 
   def child_moves(self):
-    return None if not self.is_child() else [n.move for n in self.children]
+    return None if self.is_leaf else self.attr_list('move')
 
   def __str__(self):
     s = ''
     for pre, _, node in RenderTree(self):
-      s += f"{pre}{node.name} (val = {node.value}, n_visits={node.n_visits},parent={node.parent.name if node.parent is not None else ''})\n"
+      s += f"{pre}{node.name} (val = {node.value}, n_visits={node.n_visits},"
+      s += "parent={node.parent.name if node.parent is not None else ''})\n"
     return s
